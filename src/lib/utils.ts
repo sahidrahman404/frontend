e33d5ecc06f8d001c805e67c1d2b04f4a7dbf3f4
Sahid Rahman <sahidrahman404@gmail.com>
2024-08-11 14:53:58 +0700

    add utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { z } from "zod";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const errorResponseSchema = z.object({
+  msg: z.string(),
+});
